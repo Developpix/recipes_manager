85b6612c7f55982227939bc0578b27c5240ffafd
Developpix <developpix@gmail.com>
2018-06-27 14:42:51 +0200

    Create a CookBook which represent the model. A CookBook contains a list of Pages which represent a recipe with all informations about this recipe.

---
+++ src/model/CookBook.java
@@ -0,0 +1,96 @@
+package model;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.AbstractListModel;
+
+import model.database.BD.SessionDatabase;
+import model.database.Bean.Recipe;
+import model.database.DAO.DAORecipe;
+import model.files.Page;
+
+public class CookBook extends AbstractListModel<Page> {
+
+	private String dataMod;
+	private SessionDatabase sessionDatabase;
+	private List<Page> listOfPages;
+	private File saveFile;
+	
+	/**
+	 * Constructor to create a CookBook with data from a database
+	 * @param sessionDatabase
+	 */
+	public CookBook(SessionDatabase sessionDatabase) {
+		
+		this.dataMod = "DATABASE";
+		this.listOfPages = new LinkedList<>();
+		this.sessionDatabase = sessionDatabase;
+		
+	}
+	
+	/**
+	 * Method to create a CookBook with data from a JSON file
+	 * @param fileURI the URI of the file
+	 */
+	public CookBook(String fileURI) {
+
+		this.dataMod = "FILE";
+		this.saveFile = new File(fileURI);
+		
+	}
+
+	/**
+	 * Method to get a Page at the position specify in the CookBook
+	 * @param index the position of the Page you want to get in the CookBook
+	 * @return the Page at the position
+	 */
+	@Override
+	public Page getElementAt(int index) {
+		
+		if(listOfPages.size() <= 0)
+			return null;
+		else
+			return listOfPages.get(index);
+		
+	}
+
+	/**
+	 * Method to get the number of Pages store in the CookBook
+	 * @return the number of Pages
+	 */
+	@Override
+	public int getSize() {
+		
+		return listOfPages.size();
+		
+	}
+	
+	/**
+	 * Method to load data from the file/database associated to the CookBook
+	 * if is possible
+	 */
+	public void load() {
+		
+		if(this.dataMod.equals("DATABASE")) {
+			
+			DAORecipe daoRecipe = new DAORecipe(this.sessionDatabase);
+			List<Recipe> listOfRecipes = daoRecipe.read();
+			
+			for(Recipe recipe : listOfRecipes) {
+			
+				listOfPages.add(new Page(recipe, this.sessionDatabase));
+				System.out.println(this.listOfPages.get(this.listOfPages.size()-1));
+				
+			}
+			
+		} else if (this.dataMod.equals("FILE")) {
+			
+			// TODO
+			
+		}
+		
+	}
+	
+}
